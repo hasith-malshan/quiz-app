f65bf11536f2884a6116c8206437000cb2c25ccc
hmr53 <h.malshan.53@gmail.com>
2024-04-09 16:17:42 +0530

    QuestionDao created and Modification Related to JPA added.

--- src/main/java/com/hasith/quizapp/service/QuestionService.java
+++ src/main/java/com/hasith/quizapp/service/QuestionService.java
@@ -1,16 +1,23 @@
 package com.hasith.quizapp.service;
 
+import com.hasith.quizapp.dao.QuestionDao;
 import com.hasith.quizapp.entities.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 public class QuestionService {
+
+    private QuestionDao questionDao;
+    @Autowired
+    public QuestionService(QuestionDao questionDao) {
+        this.questionDao = questionDao;
+    }
+
     public List<Question> getAllQuestions() {
-        return Collections.emptyList();
+        return questionDao.findAll();
     }
 }
