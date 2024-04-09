51ffe423b87c01d9574347fb564005ec385af942
hmr53 <h.malshan.53@gmail.com>
2024-04-09 16:04:40 +0530

    QuestionDao created and Modification Related to JPA added.

--- src/main/java/com/hasith/quizapp/dao/QuestionDao.java
+++ src/main/java/com/hasith/quizapp/dao/QuestionDao.java
@@ -1,4 +1,8 @@
 package com.hasith.quizapp.dao;
 
-public class QuestionDao {
-}
+import com.hasith.quizapp.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question,Integer> {}
