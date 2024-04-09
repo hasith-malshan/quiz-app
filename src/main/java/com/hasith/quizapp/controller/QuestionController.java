c4c894e9d1404c28c16d196014365f2b755032c5
hmr53 <h.malshan.53@gmail.com>
2024-04-09 15:42:26 +0530

    QuestionService Autowired.

--- src/main/java/com/hasith/quizapp/controller/QuestionController.java
+++ src/main/java/com/hasith/quizapp/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package com.hasith.quizapp.controller;
 
+import com.hasith.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("questions")
 public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @GetMapping("allQuestions")
     public String quiz(){
