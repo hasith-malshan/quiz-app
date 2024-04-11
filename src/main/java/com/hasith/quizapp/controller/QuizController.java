b7490d47e07e9093b05995437356e57a6e419c6e
hmr53 <h.malshan.53@gmail.com>
2024-04-11 19:40:57 +0530

    Query added in QuestionDao.

--- src/main/java/com/hasith/quizapp/controller/QuizController.java
+++ src/main/java/com/hasith/quizapp/controller/QuizController.java
@@ -1,6 +1,7 @@
 package com.hasith.quizapp.controller;
 
 import com.hasith.quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
 
     QuizService quizService;
+
+    @Autowired
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
 
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
