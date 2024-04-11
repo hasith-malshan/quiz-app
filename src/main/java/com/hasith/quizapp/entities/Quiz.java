79f260d720366131ad8bc0045d1bc9e89ea53ddd
hmr53 <h.malshan.53@gmail.com>
2024-04-11 19:09:32 +0530

    Quiz and QuizDao created

---
+++ src/main/java/com/hasith/quizapp/entities/Quiz.java
@@ -0,0 +1,4 @@
+package com.hasith.quizapp.entities;
+
+public class Quiz {
+}
--- src/main/java/com/hasith/quizapp/service/QuizService.java
+++ src/main/java/com/hasith/quizapp/service/QuizService.java
@@ -1,4 +1,11 @@
 package com.hasith.quizapp.service;
 
+import com.hasith.quizapp.dao.QuizDao;
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuizService {
-}
+
+    QuizDao quizDao;
+
+ }
